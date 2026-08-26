a0dcb7463a34631b20e657f2853581c391066bf9
Sharif <sharif@Sharifs-MacBook-Air.local>
2026-08-26 16:04:43 +0600

    settings updated

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -906,16 +906,4 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
    include $app_root . '/' . $site_path . '/settings.local.php';
  }
-$databases['default']['default'] = array (
-  'database' => 'immofirst',
-  'username' => 'root',
-  'password' => '',
-  'prefix' => '',
-  'host' => '127.0.0.1',
-  'port' => '3306',
-  'isolation_level' => 'READ COMMITTED',
-  'driver' => 'mysql',
-  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
-  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
-);
-$settings['config_sync_directory'] = 'sites/default/files/config_YqC7n15PoL-9FYichsrbLBKj7gZA-t0rDiJdiTdv4EeytdKGNE1uiy9krrZzywqybGXrWlv3lw/sync';
+
